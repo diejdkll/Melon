e312df76e015c42d562ad95f53f1a69fb6d84feb
diejbkll <s22026@gsm.hs.kr>
2023-02-08 23:06:47 +0900

    recyclerview 연결

--- app/src/main/java/com/example/melon/MelonActivity.kt
+++ app/src/main/java/com/example/melon/MelonActivity.kt
@@ -1,15 +1,20 @@
 package com.example.melon
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.Serializable
 
 class MelonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +32,17 @@ class MelonActivity : AppCompatActivity() {
                 call: Call<ArrayList<MelonItem>>,
                 response: Response<ArrayList<MelonItem>>
             ) {
-
+                if(response.isSuccessful){
+                    val melonItemList = response.body()
+                    findViewById<RecyclerView>(R.id.melon_recycler).apply {
+                        this.adapter = MelonItemRecyclerAdepter(
+                            melonItemList!!,
+                            LayoutInflater.from(this@MelonActivity),
+                            Glide.with(this@MelonActivity),
+                            this@MelonActivity
+                        )
+                    }
+                }
             }
 
             override fun onFailure(call: Call<ArrayList<MelonItem>>, t: Throwable) {
@@ -45,14 +60,28 @@ class MelonItemRecyclerAdepter(
 ): RecyclerView.Adapter<MelonItemRecyclerAdepter.Viewholder>(){
 
     inner class Viewholder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val title: TextView
+        val thumbnail: ImageView
+        val play: ImageView
 
+        init {
+            title = itemView.findViewById(R.id.title)
+            thumbnail = itemView.findViewById(R.id.thumbnail)
+            play = itemView.findViewById(R.id.play)
+
+
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Viewholder {
-
+        return Viewholder(
+            inflater.inflate(R.layout.melon_item, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: Viewholder, position: Int) {
+        holder.title.text = melonItemList.get(position).title
+        glide.load(melonItemList.get(position).thumbnail).centerCrop().into(holder.thumbnail)
     }
 
     override fun getItemCount(): Int {
