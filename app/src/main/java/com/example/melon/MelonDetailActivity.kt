e318e5e2573831d21de72b9d8488d4557b986e9b
diejbkll <s22026@gsm.hs.kr>
2023-02-09 21:43:26 +0900

    play & pause click change

--- app/src/main/java/com/example/melon/MelonDetailActivity.kt
+++ app/src/main/java/com/example/melon/MelonDetailActivity.kt
@@ -2,12 +2,38 @@ package com.example.melon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MelonDetailActivity : AppCompatActivity() {
+
+    lateinit var playPauseBottom: ImageView
+    var isPlaying: Boolean = false
+        set(value) {
+            if (value == true){
+                playPauseBottom.setImageDrawable(
+                    this.resources.getDrawable(R.drawable.pause, this.theme)
+                )
+            }else{
+                playPauseBottom.setImageDrawable(
+                    this.resources.getDrawable(R.drawable.play, this.theme)
+                )
+            }
+            field = value
+        }
+    lateinit var melonItemList: ArrayList<MelonItem>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_melon_detail)
 
-        val melonItemList = intent.getSerializableExtra("melon_item_list") as ArrayList<MelonItem>
+        playPauseBottom = findViewById(R.id.play)
+        playPauseBottom.setOnClickListener {
+            if(isPlaying == true){
+                isPlaying = false
+            }else{
+                isPlaying = true
+            }
+        }
+
+        melonItemList = intent.getSerializableExtra("melon_item_list") as ArrayList<MelonItem>
     }
 }
